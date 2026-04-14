88a1b2969c4434f3249e708a620bb04ef4c9971d
Mr_Fire_ <vadimzhunda@gmail.com>
2026-04-14 19:20:20 +0300

    исправление примера

--- examples/example_advanced.py
+++ examples/example_advanced.py
@@ -3,7 +3,7 @@ from config_advanced import CONFIG_ADVANSED
 
 result = process_log_csv(
     CONFIG_ADVANSED, 
-    ["data/log_file.csv"], # можно загружать несколько файлов
+    ["data/log_file1.csv", "data/log_file2.csv"], # можно загружать несколько файлов
     ["data/SUPERDUPLEX_HALFBODY_11_02.JBI"] # и также дополнительные файлы, которые будут скопированы к остольным результатам 
     )
 print(result)
